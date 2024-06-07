0896d23a9b97128ef413f360b5ebf0a660c1c312
MaryamMozaffari99@gmail.com <108430847+MaryamMozaffari99@users.noreply.github.com>
2024-06-08 01:41:38 +0330

    My Project in SheCodes
    I am working on a project in shecodes and this is gonna be fun :)

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-alert("")
+function search(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-input");
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = searchInputElement.value;
+  }
+  
+  function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let day = date.getDay();
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+  
+    let formattedDay = days[day];
+    return `${formattedDay} ${hours}:${minutes}`;
+  }
+  
+  let searchForm = document.querySelector("#search-form");
+  searchForm.addEventListener("submit", search);
+  
+  let currentDateELement = document.querySelector("#current-date");
+  let currentDate = new Date();
+  
+  currentDateELement.innerHTML = formatDate(currentDate);
+  
+  function displayTemperature(response) {
+    let temperature = Math.round(response.data.temperature.current);
+    let city = response.data.city;
+  
+    document.querySelector("#current-city").innerHTML = city;
+    document.querySelector("#temperature-value").innerHTML = temperature;
+  }
+  
+  function searchCity(event) {
+    event.preventDefault();
+    let city = document.querySelector("#search-input").value;
+    let apiKey = "3f030t2o7feaa60407b96a4d72ea213e";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+  
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", searchCity);
+  
